2442749069e0679f81b25be0ac9b63029d17dd76
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-02-03 15:51:39 +0300

    d

--- script.js
+++ script.js
@@ -9,33 +9,6 @@ if ('serviceWorker' in navigator) {
 		})
 	})
 }
-// const btnAdd = document.createElement('button')
-// btnAdd.innerHTML = 'Add to home screen'
-// document.body.append(btnAdd)
-// let deferredPrompt;
-
-// window.addEventListener('beforeinstallprompt', (e) => {
-// 	e.preventDefault();
-// 	deferredPrompt = e;
-// 	btnAdd.style.display = 'block';
-// });
-// btnAdd.addEventListener('click', (e) => {
-// 	btnAdd.style.display = 'none';
-// 	deferredPrompt.prompt();
-// 	deferredPrompt.userChoice
-// 		.then((choiceResult) => {
-// 			if (choiceResult.outcome === 'accepted') {
-// 				console.log('User accepted the A2HS prompt');
-// 			} else {
-// 				console.log('User dismissed the A2HS prompt');
-// 			}
-// 			deferredPrompt = null;
-// 		});
-// });
-// window.addEventListener('appinstalled', (evt) => {
-// 	console.log('a2hs installed');
-// 	app.logEvent('a2hs', 'installed');
-// });
 
 var user = {
 	gender:"",
