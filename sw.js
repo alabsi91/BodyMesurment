285838f8cfd625f905d68e23b2a18dfb758769f8
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-02-03 14:49:39 +0300

    s

---
+++ sw.js
@@ -0,0 +1,35 @@
+
+const cacheName = 'cache-v1'
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'style.css',
+    'b1.jpg',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.googleapis.com/css?family=Fira+Sans&display=swap',
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+]
+
+
+self.addEventListener('install', event => {
+    console.log('Install event');
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(resourcesToPrecache)
+        })
+    )
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activate event')
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for ', event.request.url)
+    event.respondWith(caches.match(event, request)
+    .then(cachedResponse => {
+        return cachedResponse || fetch(event.request)
+    })
+    )
+})
