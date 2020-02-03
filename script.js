a77c3bff75fb69d9ffbbcd9a53ccc8c8798a3cef
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-02-03 15:40:14 +0300

    u

--- script.js
+++ script.js
@@ -9,33 +9,33 @@ if ('serviceWorker' in navigator) {
 		})
 	})
 }
-const btnAdd = document.createElement('button')
-btnAdd.innerHTML = 'Add to home screen'
-document.body.append(btnAdd)
-let deferredPrompt;
+// const btnAdd = document.createElement('button')
+// btnAdd.innerHTML = 'Add to home screen'
+// document.body.append(btnAdd)
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-	e.preventDefault();
-	deferredPrompt = e;
-	btnAdd.style.display = 'block';
-});
-btnAdd.addEventListener('click', (e) => {
-	btnAdd.style.display = 'none';
-	deferredPrompt.prompt();
-	deferredPrompt.userChoice
-		.then((choiceResult) => {
-			if (choiceResult.outcome === 'accepted') {
-				console.log('User accepted the A2HS prompt');
-			} else {
-				console.log('User dismissed the A2HS prompt');
-			}
-			deferredPrompt = null;
-		});
-});
-window.addEventListener('appinstalled', (evt) => {
-	console.log('a2hs installed');
-	app.logEvent('a2hs', 'installed');
-});
+// window.addEventListener('beforeinstallprompt', (e) => {
+// 	e.preventDefault();
+// 	deferredPrompt = e;
+// 	btnAdd.style.display = 'block';
+// });
+// btnAdd.addEventListener('click', (e) => {
+// 	btnAdd.style.display = 'none';
+// 	deferredPrompt.prompt();
+// 	deferredPrompt.userChoice
+// 		.then((choiceResult) => {
+// 			if (choiceResult.outcome === 'accepted') {
+// 				console.log('User accepted the A2HS prompt');
+// 			} else {
+// 				console.log('User dismissed the A2HS prompt');
+// 			}
+// 			deferredPrompt = null;
+// 		});
+// });
+// window.addEventListener('appinstalled', (evt) => {
+// 	console.log('a2hs installed');
+// 	app.logEvent('a2hs', 'installed');
+// });
 
 var user = {
 	gender:"",
