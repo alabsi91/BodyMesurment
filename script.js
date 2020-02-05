569e0d6a58884104dcea5eb01c99a4c6aaa299e5
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-02-05 22:55:21 +0300

    k

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
 });
 document.getElementById('addToHomebutt').addEventListener('click', (e) => {
 	deferredPrompt.prompt();
-	deferredPrompt.userChoice
-		.then((choiceResult) => {
-			if (choiceResult.outcome === 'accepted') {
-				console.log('User accepted the A2HS prompt');
-			} else {
-				console.log('User dismissed the A2HS prompt');
-			}
-			deferredPrompt = null;
-		});
-});
+// 	deferredPrompt.userChoice
+// 		.then((choiceResult) => {
+// 			if (choiceResult.outcome === 'accepted') {
+// 				console.log('User accepted the A2HS prompt');
+// 			} else {
+// 				console.log('User dismissed the A2HS prompt');
+// 			}
+// 			deferredPrompt = null;
+// 		});
+// });
 const user = {
 	gender: "",
 	age: 0,
