9133758d6bfcd71da34ef29560f3497880529530
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-02-05 16:21:55 +0300

    l

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ if ('serviceWorker' in navigator) {
 		})
 	})
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+});
+document.getElementById('topage1').addEventListener('click', (e) => {
+	
+	// Show the prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+});
 var user = {
 	gender:"",
 	age:0,
